0a614c572ccd3e371183424ddc21c0d2fd4e4b9d
Nayeem <mahfujurrahamannayeem@gmail.com>
2024-08-27 21:53:35 +0600

    Completing The Ellipse And Rombhosh

--- Scripts/Area-Calculator.js
+++ Scripts/Area-Calculator.js
@@ -42,3 +42,56 @@ function calculateRectangleArea(){
     const rectangleAreaSpan = document.getElementById('rectangle-area');
     rectangleAreaSpan.innerText = rectangleArea;
 }
+
+//Reuseable Function For Reducing The Code
+
+function calculateParallelogramArea(){
+const base = getInputValue('parallelogram-base');
+const height = getInputValue('parallelogram-height')
+//Finding the Parallelogram Area
+const parallelogramArea = base * height ;
+//Set the elements calutaed area 
+setElementsInnerText('parallelogram-area',parallelogramArea)
+}
+
+// Finding The Rombhosh Area 
+function calculateRombhoshArea(){
+    const firstDiagonal = getInputValue('first-diagonal');
+    const secondDiagonal = getInputValue('second-diagonal');
+    // 
+    const rombhoshArea = 0.5 * firstDiagonal * secondDiagonal ;
+     
+    //Set the elements Area text 
+    setElementsInnerText('rhombosh-area' , rombhoshArea)
+}
+
+
+//Finding The Ellipse Area 
+ function calculateEllipseArea(){
+    // Getting The Major And Minor Radius
+
+    const majorRadius = getInputValue('major-radius');
+    const minorRadius = getInputValue('minor-radius');
+
+    // Getting The Ellipse Area
+
+    const ellipsArea = 3.141 * majorRadius * minorRadius ;
+
+    //Set the Area inner Text 
+    setElementsInnerText('ellipse-area', ellipsArea)
+ }
+
+//Getting the Reuseable input Feild via Function
+function getInputValue(feildId){
+    const inputFeild = document.getElementById(feildId);
+    const inputFeildText = inputFeild.value;
+    const value = parseFloat(inputFeildText);
+    return value;
+}
+
+// reuseable set span,div,p,etc text 
+
+function setElementsInnerText(elementId,khetrofol){
+    const element  = document.getElementById(elementId);
+   element.innerText = khetrofol;
+}
